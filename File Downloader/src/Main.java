5bd0da8f9f45a1e52cf5b6a86bb80f11886ca0a0
Roman Syunyakov <romansunyakov@gmail.com>
2019-09-30 15:28:12 +0300

    update

--- File Downloader/src/Main.java
+++ File Downloader/src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String[] s1 = new String[1];
-        s1[0] = "https://maven.apache.org/guides/introduction/introduction-to-the-standard-directory-layout.html";
-        FileDownloader downloader = new FileDownloader(s1);
+        FileDownloader downloader = new FileDownloader(args);
         downloader.download();
     }
 }
